a94f613ccdfcc1a74d3b8fe2198781d60b217750
Chris Borowski <krzysztofeborowski@gmail.com>
2023-06-12 23:12:50 -0500

    Added user-specified number of characters

--- pass123.py
+++ pass123.py
@@ -5,11 +5,12 @@ import string
 
 # generate random alphanumeric password
 
-password_length = 20
+password_length = int(input("How many characters should there be in your password? "))
 
 new_password = ''.join(random.choices(string.ascii_letters + string.punctuation + string.digits, k = password_length))
 
-print(str(new_password))
+print(f"Your new {password_length}-character password is: {new_password}.")
+
 
 # copy password to clipboard
 
